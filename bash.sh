b7979181fc11a8cd0e25ac74e9dc11bbca8215c9
CraftyChimera <viswanathiyyappan@gmail.com>
2021-08-25 09:54:35 +0530

    Almost done

--- bash.sh
+++ bash.sh
@@ -3,18 +3,24 @@ while read line;
 do
 echo $line > a
 name=$(awk 'BEGIN { FS=","} {print $1}' a)
+name2=${name// /_}
+name=${name2}
 eid=$(awk 'BEGIN { FS=","} {print $2}' a)
 phone=$(awk 'BEGIN { FS=","} {print $3}' a)
 echo $(grep -w "${eid}" file2.txt| head -1) > row
 echo $(grep -w "${phone}" file3.txt| head -1) > row2
-if [ $(grep -w "${eid}" file2.txt| wc -l) -eq 0 ]
-then
-printf "%-15s \t %-17s \t %-10s \t %-15s \t %-19s \t %-22s \n" $eid "$name" $phone " " $(awk 'BEGIN { FS=","} {print $1}' row2 ) $(awk 'BEGIN { FS=","} {print $3}' row2 ) >> merged.txt
-else
-printf "%-15s \t %-17s \t %-10s \t %-15s \t %-15s \t %-22s \n" $eid "$name" $phone $(awk 'BEGIN { FS=","} {print $2}' row ) $(awk 'BEGIN { FS=","} {print $1}' row2 ) $(awk 'BEGIN { FS=","} {print $3}' row2 ) >> merged.txt
-fi
+sal=$(awk 'BEGIN { FS=","} {print $2}' row )
+salary=${sal:-"(NULL)"}
+em=$(awk 'BEGIN { FS=","} {print $1}' row2 )
+email=${em:-"(NULL)"}
+yr=$(awk 'BEGIN { FS=","} {print $3}' row2 )
+year=${yr:-"(NULL)"}
+printf "%s\t%s\t%s\t%s\t%s\t%s\n" $eid "$name" $phone $salary $email $year >> merged.txt
 done < file1.txt
 sed -i 's/"/ /g' merged.txt
+column -t merged.txt >> finally
 rm a
 rm row
 rm row2
+rm merged.txt
+sed -e 's/_/ /g' finally >> merged.txt
