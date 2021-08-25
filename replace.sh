e80557c03df603381c71f0e146657e528af3ac5f
CraftyChimera <viswanathiyyappan@gmail.com>
2021-08-25 11:11:30 +0530

    almost done with replace

--- replace.sh
+++ replace.sh
@@ -1,1 +1,3 @@
-awk -v a=$1 '{for(i=1;i<NF;i++)if($i==a)m=i;{print $m}}' merged.txt
+column=$(awk -v a=$1 '{for(i=1;i<=NF;i++)if($i==a)print i}' merged.txt)
+awk -v a="$2" -v b="$3" '{gsub(a,b,$6)} 1' merged.txt > pipe
+column -t pipe>a
